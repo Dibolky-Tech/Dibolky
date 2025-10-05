e78439bcaf58f76e7b24e969fa9b0eab61ac1d2b
JASIR BASHEER <jasirbinbasheerpp@gmail.com>
2025-10-05 21:11:53 +0530

    update: about us section contents

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -9,19 +9,25 @@ export default function AboutSection() {
         <div className="tt-row">
           <div className="tt-col-lg-4">
             <div className="tt-heading tt-heading-xlg">
-              <h2 className="tt-heading-title tt-text-reveal">About Me</h2>
+              <h2 style={{ fontFamily: "'Barracuda', 'Barracuda-regular', sans-serif" }} className="tt-heading-title tt-text-reveal">ABOUT US</h2>
             </div>
-            <div className="tt-text-uppercase margin-top-30 tt-text-reveal">
+            {/* <div className="tt-text-uppercase margin-top-30 tt-text-reveal">
               Creative designer<br /> based in Melbourne
-            </div>
+            </div> */}
           </div>
 
           <div className="tt-col-lg-1 padding-top-30">
           </div>
 
           <div className="tt-col-lg-7 tt-align-self-center">
-            <div className="text-xxlg font-500 tt-text-reveal">
-              I am dedicated to developing innovative solutions and impactful experiences that meet user needs and exceed expectations.
+            <div className="text-xlg font-500 tt-text-reveal">
+              we're passionate about crafting exceptional
+digital experiences that ignite connections
+and propel brands forward. We're more than
+just a digital marketing agency; we're a team
+of enthusiastic storytellers, meticulous
+strategists, and creative minds driven by the
+desire to make a lasting impact.
             </div>
             <Link href="/about" className="tt-btn tt-btn-outline margin-top-40 tt-magnetic-item tt-anim-fadeinup">
               <span data-hover="Read More">Read More</span>
